8eb27e1204e1bb15447141f9ca3ee40e7dbe286d
Niklas K <drdaxxy@googlemail.com>
2018-02-02 12:05:45 +0100

    initial commit

--- ffxvBenchCustom/ffxvBenchCustom.cpp
+++ ffxvBenchCustom/ffxvBenchCustom.cpp
@@ -1,11 +1,46 @@
-// ffxvBenchCustom.cpp : Defines the entry point for the console application.
-//
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
 
-#include "stdafx.h"
+typedef HRESULT(WINAPI *DirectInput8CreateProc)(HINSTANCE hinst,
+                                                DWORD dwVersion, REFIID riidltf,
+                                                LPVOID *ppvOut,
+                                                LPUNKNOWN punkOuter);
 
+static DirectInput8CreateProc realDirectInput8Create = NULL;
+static HINSTANCE hRealDinput8 = NULL;
 
-int main()
-{
-    return 0;
+static bool patched = false;
+
+void patch() {}
+
+#pragma comment(linker, "/EXPORT:DirectInput8Create=ProxyDirectInput8Create")
+extern "C" HRESULT WINAPI ProxyDirectInput8Create(HINSTANCE hinst,
+                                                  DWORD dwVersion,
+                                                  REFIID riidltf,
+                                                  LPVOID *ppvOut,
+                                                  LPUNKNOWN punkOuter) {
+  if (!hRealDinput8) {
+    TCHAR expandedPath[MAX_PATH];
+    ExpandEnvironmentStrings(L"%WINDIR%\\System32\\dinput8.dll", expandedPath,
+                             MAX_PATH);
+    hRealDinput8 = LoadLibrary(expandedPath);
+    if (!hRealDinput8)
+      return DIERR_OUTOFMEMORY;
+    realDirectInput8Create = (DirectInput8CreateProc)GetProcAddress(
+        hRealDinput8, "DirectInput8Create");
+    if (!realDirectInput8Create)
+      return DIERR_OUTOFMEMORY;
+  }
+  return realDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 }
 
+BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason,
+                       IN LPVOID Reserved) {
+  if (nReason == DLL_PROCESS_ATTACH) {
+    if (!patched) {
+      patch();
+      patched = true;
+    }
+  }
+  return TRUE;
+}
